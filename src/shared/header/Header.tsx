6215dbea9bae47c545ad66292b0d084235abd156
wooluck <155030236+parkwoohyeok@users.noreply.github.com>
2024-10-16 03:32:21 +0900

    커밋

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -8,12 +8,10 @@ export default function Header() {
   const LIST_DEFAULT_STYLE = `h-[2px] bg-[#0058a7] w-0 absolute -top-[25px] transition-all duration-500 group-hover:w-full`
 
   useEffect(() => {
-    // Header가 보이도록 지연 시간 설정
     const timer = setTimeout(() => {
       setShowHeader(true)
     }, 300)
 
-    // GARION 텍스트 애니메이션을 위한 타이머
     const titleTimer = setTimeout(() => {
       setShowTitle(true)
     }, 600)
@@ -32,7 +30,6 @@ export default function Header() {
     >
       <div className="flex items-center hover:cursor-pointer h-full gap-[5px]">
         <Image src={logo} width={50} height={32} alt="회사로고" />
-        {/* GARION 텍스트에 애니메이션 추가 */}
         <h1
           className={`text-[30px] transition-all duration-1000 transform ${
             showTitle
